cc036f257bae458372b5642d66a8f081d087785c
Lioneriod <123819366+Lioneriod@users.noreply.github.com>
2024-04-11 16:38:06 -0300

    separate numbers in arrays to check it later

--- script.js
+++ script.js
@@ -26,21 +26,44 @@ const plateConvert = {
   y: "25",
   z: "26",
 };
-const plateInput1 = "AAA2045";
+
+const statesStartAM = {
+  AM1: [10, 23, 6],
+  AM2: [14, 15, 9],
+  AM3: [15, 1, 1],
+  AM4: [15, 24, 13],
+  AM5: [16, 8, 1],
+  AM6: [17, 26, 1],
+};
+
+const statesEndAM = {
+  AM1: [10, 24, 25],
+  AM2: [14, 16, 2],
+  AM3: [15, 1, 15],
+  AM5: [16, 8, 26],
+  AM6: [17, 26, 26],
+};
+
+const plateInput1 = "JWF2045";
 const plateInput2 = "BEZ2045";
 const plateInput3 = "BFA2045";
 const plateInput4 = "GKI2045";
 
 function stateId(plate) {
-  let plateSum = "";
+  let plateSum = [];
   for (let i = 0; i < 3; i++) {
     let plateNumber = plateConvert[plate[i].toLowerCase()];
-    plateSum += plateNumber;
+    plateSum.push(plateNumber);
   }
-  if (plateSum.length == 4) {
-    plateSum[3] == "";
+  plateSumNum = plateSum.map(Number);
+  for (const statesStartAMKey of Object.keys(statesStartAM)) {
+    if (plateSumNum[0] === statesStartAM[statesStartAMKey][0]) {
+      matchedKey = statesStartAMKey; // Store the matched key
+      console.log(`The first element of ${statesStartAMKey} matches.`);
+      // Do something if the condition is met
+    }
+    console.log("Matched key:", matchedKey);
   }
-  return plateSum;
 }
 
 console.log(
