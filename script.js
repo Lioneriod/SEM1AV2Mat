b8643f85e9a9ebe933155ad68f8b1c02af15ab35
Lioneriod <123819366+Lioneriod@users.noreply.github.com>
2024-04-14 14:17:20 -0300

    add a else part for the plates that don't match

--- script.js
+++ script.js
@@ -41,7 +41,10 @@ const statesStartObj = {
       [15, 1, 1],
       [15, 1, 15],
     ],
-    AM4: [[15, 24, 13]],
+    AM4: [
+      [15, 24, 13],
+      [15, 24, 13],
+    ],
     AM5: [
       [16, 8, 1],
       [16, 8, 26],
@@ -60,8 +63,14 @@ const statesStartObj = {
       [14, 24, 18],
       [14, 24, 20],
     ],
-    AC3: [[15, 22, 7]],
-    AC4: [[15, 24, 16]],
+    AC3: [
+      [15, 22, 7],
+      [15, 22, 7],
+    ],
+    AC4: [
+      [15, 24, 16],
+      [15, 24, 16],
+    ],
     AC5: [
       [17, 12, 21],
       [17, 12, 26],
@@ -80,8 +89,14 @@ const statesStartObj = {
       [15, 8, 12],
       [15, 8, 23],
     ],
-    RO3: [[15, 24, 12]],
-    RO4: [[17, 18, 1]],
+    RO3: [
+      [15, 24, 12],
+      [15, 24, 12],
+    ],
+    RO4: [
+      [17, 18, 1],
+      [17, 18, 1],
+    ],
     RO5: [
       [17, 20, 1],
       [17, 20, 10],
@@ -93,44 +108,63 @@ const statesStartObj = {
   },
 };
 
-const plateInput1 = "JWF2045";
-const plateInput2 = "OXM2045";
-
+const plateInput1 = "OXM2045";
+const plateInput2 = "ZZZ2045";
 function stateId(plate) {
+  function stateNamer(x, y, z) {
+    switch (z) {
+      case "statesIdAM":
+        x = "Amazonas";
+        console.log(`A placa é do estado do ${x} na faixa ${y}`);
+        break;
+      case "statesIdAC":
+        x = "Acre";
+        console.log(`A placa é do estado do ${x} na faixa ${y}`);
+        break;
+      case "statesIdRO":
+        x = "Rondônia";
+        console.log(`A placa é do estado de ${x} na faixa ${y}`);
+        break;
+    }
+  }
   let plateSum = [];
+  let plateCut = plate.slice(0, 3);
   for (let i = 0; i < 3; i++) {
-    let plateNumber = plateConvert[plate[i].toLowerCase()];
+    let plateNumber = plateConvert[plateCut[i].toLowerCase()];
     plateSum.push(plateNumber);
   }
   plateSumNum = plateSum.map(Number);
   for (const stateStartKey in statesStartObj) {
+    let stateName = "";
     const stateObj = statesStartObj[stateStartKey];
     for (const stateKey in stateObj) {
-      let stateName = "";
-      if (plateSumNum[0] === stateObj[stateKey][0][0]) {
+      if (plateSumNum[0] == stateObj[stateKey][0][0]) {
         //stateStartKey = The key for the state object
         //stateKey = The key containing the fist number that matched with the plate
-        if (stateKey == "AM4") {
-          //Since AM4 got only 1 set of numbers, we define it first
-          if (plateSumNum[1] === stateObj[stateKey][0][1]) {
-            if (plateSumNum[2] === stateObj[stateKey][0][2])
-              console.log("Teste AM4");
-          }
+        if (plateSumNum.toString() == stateObj[stateKey][0].toString()) {
+          return stateNamer(stateName, stateKey, stateStartKey);
         } else if (
-          plateSumNum[1] >= stateObj[stateKey][0][1] &&
-          plateSumNum[1] <= stateObj[stateKey][1][1]
+          plateSumNum[0] >= stateObj[stateKey][0][0] &&
+          plateSumNum[0] <= stateObj[stateKey][1][0]
         ) {
-          switch (stateStartKey) {
-            case "statesIdAM":
-              stateName = "Amazonas";
+          if (
+            plateSumNum[1] >= stateObj[stateKey][0][1] &&
+            plateSumNum[1] <= stateObj[stateKey][1][1]
+          ) {
+            if (
+              plateSumNum[2] >= stateObj[stateKey][0][2] &&
+              plateSumNum[2] <= stateObj[stateKey][1][2]
+            ) {
+              return stateNamer(stateName, stateKey, stateStartKey);
+            }
           }
-          console.log(
-            `A placa é do estado do ${stateName} na faixa ${stateKey}`
-          );
         }
       }
     }
   }
+  // If no state is found, return a message
+  console.log("Essa placa não pertence a nenhum estado da região Norte 1");
 }
 
-console.log(stateId(plateInput1), stateId(plateInput2));
+stateId(plateInput1);
+stateId(plateInput2);
